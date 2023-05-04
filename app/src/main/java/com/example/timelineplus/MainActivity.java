e148be5334b44d8ceb7130d9dc69d07e1183d7ce
Alyssa Vivien <alyssavivien.sc@gmail.com>
2023-05-04 22:13:40 +0800

    Log in and Register

--- app/src/main/java/com/example/timelineplus/MainActivity.java
+++ app/src/main/java/com/example/timelineplus/MainActivity.java
@@ -1,34 +1,61 @@
 package com.example.timelineplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnRegister = findViewById(R.id.btnRegister);
-        Button btnLogin = findViewById(R.id.btnLogin);
+        text = (TextView) findViewById(R.id.signin);
+        Button btnLogin = findViewById(R.id.login);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        /*SpannableString span = new SpannableString("Sign In");
+        ClickableSpan clickableSpan = new ClickableSpan() {
             @Override
-            public void onClick(View v) {
-                Intent register = new Intent(MainActivity.this, Register.class);
-                startActivity(register);
+            public void onClick(View view) {
+                Intent signin = new Intent(MainActivity.this, Register.class);
+                startActivity(signin);
+            }
+        };
+
+        span.setSpan(new ForegroundColorSpan(Color.WHITE), 0, 7, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        TextView textView = findViewById(R.id.signin);
+        textView.setText(span);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());*/
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signin = new Intent(MainActivity.this, Register.class);
+                startActivity(signin);
             }
         });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent login = new Intent(MainActivity.this, Login.class);
+                Intent login = new Intent(MainActivity.this, Home.class);
                 startActivity(login);
             }
         });
