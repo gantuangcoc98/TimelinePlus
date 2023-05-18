fedc1ee3f2cb7e0961f331bc225e6b757840b8ad
gantuangcoc98 <gantuangco98@yahoo.com.ph>
2023-05-18 19:48:27 +0800

    Updated to the latest Edit in Add Post Feature

--- app/src/main/java/com/example/timelineplus/FragmentAddPost.java
+++ app/src/main/java/com/example/timelineplus/FragmentAddPost.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class FragmentAddPost extends Fragment {
 
@@ -14,13 +15,29 @@ public class FragmentAddPost extends Fragment {
         // Required empty public constructor
     }
 
+    private EditText setLocation;
+    private EditText setEmail;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_post, container, false);
 
-        
+        setLocation = view.findViewById(R.id.setLocation);
+        setEmail = view.findViewById(R.id.setEmail);
+        setLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLocation.setText("");
+            }
+        });
+
+        setEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setEmail.setText("");
+            }
+        });
 
         return view;
     }
