47dc4d65ec53a7ea199db8819a0b7fb9a32fbdf1
Alyssa Vivien <alyssavivien.sc@gmail.com>
2023-05-24 22:11:25 +0800

    Darkmode and final polishes

--- app/src/main/java/com/example/timelineplus/FragmentSettings.java
+++ app/src/main/java/com/example/timelineplus/FragmentSettings.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +24,8 @@ public class FragmentSettings extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Switch switcher;
 
     public FragmentSettings() {
         // Required empty public constructor
@@ -53,6 +56,8 @@ public class FragmentSettings extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        //switcher = findViewById(R.id.darkmodeswitch);
     }
 
     @Override
